aa993b9a0932ced88f85dbb52da3c81e18e287bc
root <jjmalouf@maloufs.org>
2018-07-10 08:50:50 -0600

    implemented a gradient

---
+++ src/components/gradient.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class gradient extends Component {
+    render() {
+        return <div className="gradient"></div>
+    }
+}
+
+export default Gradient;
